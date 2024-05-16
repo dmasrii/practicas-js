912335437148e4baaa61cd52b0e1430ece1e6bd5
dmasrii <dmasri@udesa.edu.ar>
2024-05-16 14:39:23 -0300

    practicas terminadas

--- js/objlit1.js
+++ js/objlit1.js
@@ -4,10 +4,11 @@
 let misDatos = {
     nombre: 'Delfina',
     apellido: 'Masri',
+    edad: 19,
     dni: 46214085,
     comidasFavoritas: ['Sushi', 'Milanesas', 'Fideos'],
     saludar: function(){
-        return (`Hola mi nombre es ${this.nombre} ${this.apellido} y tengo 19 años. Mi primer comida favorita es ${this.comidasFavoritas [0]}`)
+        return (`Hola mi nombre es ${this.nombre} ${this.apellido} y tengo ${this.edad} años. Mi primer comida favorita es ${this.comidasFavoritas [0]}`)
         }
     }
 
